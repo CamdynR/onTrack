2e77ea565a092d6dbbdac64b62b726efc1c8f4c8
Kevin Rafferty <kraffert@ucsd.edu>
2019-02-28 20:53:38 -0800

    fixed the routes issue

--- app.js
+++ app.js
@@ -21,24 +21,13 @@ var clear = require('./routes/clear');
 
 //Routine 1 Pages
 var startOne = require('./routes/startOne');
-var oneR1 = require('./routes/oneR1');
-var twoR1 = require('./routes/twoR1');
-var threeR1 = require('./routes/threeR1');
-var fourR1 = require('./routes/fourR1');
 
 //Routine 2 Pages
 var startTwo = require('./routes/startTwo');
-var oneR2 = require('./routes/oneR2');
-var twoR2 = require('./routes/twoR2');
-var threeR2 = require('./routes/threeR2');
-var fourR2 = require('./routes/fourR2');
 
 //Routine 3 Pages
 var startThree = require('./routes/startThree');
-var oneR3 = require('./routes/oneR3');
-var twoR3 = require('./routes/twoR3');
-var threeR3 = require('./routes/threeR3');
-var fourR3 = require('./routes/fourR3');
+
 // Example route
 // var user = require('./routes/user');
 
@@ -79,24 +68,12 @@ app.get('/clear', clear.clearRoutine);
 
 //Routine 1 Pages
 app.get('/startOne', startOne.startOne);
-app.get('/oneR1', oneR1.oneR1);
-app.get('/twoR1', twoR1.twoR1);
-app.get('/threeR1', threeR1.threeR1);
-app.get('/fourR1', fourR1.fourR1);
 
 //Routine 2 Pages
 app.get('/startTwo', startTwo.startTwo);
-app.get('/oneR2', oneR2.oneR2);
-app.get('/twoR2', twoR2.twoR2);
-app.get('/threeR2', threeR2.threeR2);
-app.get('/fourR2', fourR2.fourR2);
 
 //Routine 3 Pages
 app.get('/startThree', startThree.startThree);
-app.get('/oneR3', oneR3.oneR3);
-app.get('/twoR3', twoR3.twoR3);
-app.get('/threeR3', threeR3.threeR3);
-app.get('/fourR3', fourR3.fourR3);
 
 // Example route
 // app.get('/users', user.list);
