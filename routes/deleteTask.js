698817ba34baf73abaea8787145a4e01f8a70cf8
Chris Wynne <cwynne@ucsd.edu>
2019-03-04 22:11:17 -0800

    working delete

--- routes/deleteTask.js
+++ routes/deleteTask.js
@@ -3,9 +3,9 @@ var data = require('../currentUsers.json');
 exports.deleteTask = function(req,res) {
 	// console.log(req.query.task);
 	var index = req.query.task;
-	var parent = req.query;
+	var parent = req.query.parentRoutine;
 	console.log("Parent");
 	console.log(parent);
-	data.Users[0].Routines[parent].Tasks.splice(index,index)
-	res.render('taskEditor', data.Users[0].Routines[parent]);
+	data.Users[0].Routines[parent].Tasks.splice(index,index+1);
+	res.render('taskEditorB', data.Users[0].Routines[parent]);
 }
