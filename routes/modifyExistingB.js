b3abfe975f8a3dcec5e944113f02211dcd6aeb77
Chris Wynne <cwynne@ucsd.edu>
2019-03-05 13:04:09 -0800

    Routine Names working

--- routes/modifyExistingB.js
+++ routes/modifyExistingB.js
@@ -2,6 +2,6 @@ var data = require('../currentUsers.json');
 
 exports.modifyExistingB = function(req,res) {
 	console.log(req.query.rout);
-	var index = req.query.rout;
+	var index = req.query.routName;
 	res.render('taskEditorB', data.Users[0].Routines[index]);
 }
