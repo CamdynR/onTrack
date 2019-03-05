30c311de55250714cc661f4859e0d8c753972c13
Chris Wynne <cwynne@ucsd.edu>
2019-03-05 12:53:43 -0800

    viewTasksB looking better

--- routes/deleteTask.js
+++ routes/deleteTask.js
@@ -2,10 +2,10 @@ var data = require('../currentUsers.json');
 
 exports.deleteTask = function(req,res) {
 	// console.log(req.query.task);
-	var index = req.query.task;
+	var index = req.query.taskIndex;
 	var parent = req.query.parentRoutine;
-	console.log("Parent");
-	console.log(parent);
+	console.log("Index");
+	console.log(index);
 	data.Users[0].Routines[parent].Tasks.splice(index,index+1);
 	res.render('taskEditorB', data.Users[0].Routines[parent]);
 }
--- routes/modifyTasksB.js
+++ routes/modifyTasksB.js
@@ -2,7 +2,7 @@ var data = require('../currentUsers.json');
 
 exports.modifyTasksB = function(req,res) {
 	var toPush = {'name': req.query.tempName, 'time': req.query.tempTime}
-	var index = req.query.indexer;
+	var index = req.query.routIndex;
 	data.Users[0].Routines[index].Tasks.push(toPush);
 	res.render('taskEditorB', data.Users[0].Routines[index]);
 }
