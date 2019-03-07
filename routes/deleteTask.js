37c1151b61c997350571080359dcbeadd254d751
Kevin Rafferty <steelerkevin7@yahoo.com>
2019-03-06 21:25:56 -0800

    Dynamic

--- routes/deleteTask.js
+++ routes/deleteTask.js
@@ -4,6 +4,7 @@ exports.deleteTask = function(req,res) {
 	// console.log(req.query.task);
 	var index = req.query.taskIndex;
 	var parent = req.query.parentRoutine;
+	var stay = req.query.stayOnPage;
 	console.log("Index");
 	console.log(index);
 	data.Users[0].Routines[parent].Tasks.splice(index,index+1);
